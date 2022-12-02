9cf334d2738b66b8a3156ccd6205d29b073d9136
John Capehart <john.capehart@organicvalley.com>
2022-12-02 09:55:32 -0600

    Add in_httptest2 based on sys.calls

--- tests/testthat/helper_test.R
+++ tests/testthat/helper_test.R
@@ -7,8 +7,7 @@ in_httptest2 <- function() {
   return(!is.null(call) && length(call) > 0)
 }
 
-upload_bo_mtcars <- function(conn, n = NULL) {
-  require(openxlsx, quietly=TRUE)
+upload_bo_mtcars <- function(conn, n = NULL, suffix = 1) {
   df <- mtcars
   if (is.null(n)) {
     n <- nrow(df)
@@ -21,7 +20,58 @@ upload_bo_mtcars <- function(conn, n = NULL) {
   df <- tibble::rownames_to_column(df, "model")
   # slice the set
   df <- head(df, n)
-  filename <- Sys.getenv('BO_TEST_EXCEL_FILE_NAME')
+  filename <- paste(Sys.getenv('BO_TEST_EXCEL_FILE_NAME'), suffix)
   folder_id = Sys.getenv('BO_TEST_FOLDER_ID')
   upload_df(conn, df, filename, folder_id, 'Cars')
 }
+
+#' Generate httptest2 mock files
+#'
+#' @return Nothing
+
+#' @example
+#' \donttest{
+#' generate_mocks()
+#' }
+#' @noRd
+generate_mocks <- function() {
+  library(httptest2)
+  devtools::load_all()
+  withr::local_dir(file.path(Sys.getenv('RENV_PROJECT'), 'tests/testthat'))
+  logger::log_threshold(WARN)
+  # remove mock directory
+  unlink('mock', recursive = TRUE)
+  with_mock_dir('mock',{
+    clear_all_tokens()# force new token
+    local_options('bo_no_new_tokens'=FALSE) #nale new token
+    conn<-open_bo_connection() # force a login with a new token
+    local_options('bo_no_new_tokens'=TRUE) # block new tokens
+    # httptest2::block_requests()
+    devtools::test()
+    # testthat::test_file('test-connect.R')
+    # testthat::test_file('test-api-utils.R')
+    # testthat::test_file('test-document.R')
+    # testthat::test_file('test-report.R')
+    # testthat::test_file('test-controls.R')
+    # testthat::test_file('test-spreadsheet.R')
+  })
+}
+
+#' Generate test_down report
+#'
+#' @return
+
+#' @example
+#' \donttest{
+#' generate_test_report()
+#' }
+#' @noRd
+generate_test_report <- function() {
+  library(httptest2)
+  library(testdown)
+  devtools::load_all()
+
+  logger::log_threshold(WARN)
+  testdown::test_down(devtools::test())
+}
+
